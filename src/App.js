b645f8cb203746b19e4130aa2362ce0eaf05732b
jeewon94 <jayyy1224@gmail.com>
2021-04-01 15:52:51 +0900

    04/01(목) 리액트 테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class Greeting extends Component{
+  
+  //render 함수의 내부에 return 갑은 반드시 하나의 태그로 감싸져야 한다.
+  render(){
+    return(
+      <div>
+        <p>Hello React Again!</p>
+        
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <p>Hello React!</p>
+      <Greeting />
     </div>
   );
 }
